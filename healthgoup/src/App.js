980f23661b00770bbed8dae2e01a1d0301ea8507
Alan Wang <alanwang123919@gmail.com>
2023-06-23 16:35:05 -0700

    set up react and chakra

---
+++ healthgoup/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@chakra-ui/react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Box>
+        <h1>hi</h1>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
