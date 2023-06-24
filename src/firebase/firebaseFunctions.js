b821fcc8a81ae812fa652d90e9fda2819f71087d
Byron Wang <byronwang123919@gmail.com>
2023-06-24 15:06:40 -0700

    add firebase util functions

---
+++ src/firebase/firebaseFunctions.js
@@ -0,0 +1,67 @@
+import {
+  addDoc,
+  getDoc,
+  deleteDoc,
+  getDocs,
+  updateDoc,
+  doc,
+  collection,
+  setDoc,
+} from "@firebase/firestore";
+import { db } from "./config.js";
+
+const getUserData = async (id) => {
+  const docRef = doc(db, "users", id);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log("it exists");
+    return docSnap.data();
+  } else {
+    return null;
+  }
+};
+
+const addUser = async (uid, name, email, photoURL) => {
+  const docSnap = getUserData(uid);
+  if (docSnap) {
+    let data = {
+      email: email,
+      name: name,
+      firstName: name.split(" ")[0],
+      uid: uid,
+      photoURL: photoURL,
+      posts: [],
+    };
+
+    const userRef = doc(db, "users", uid);
+    try {
+      await setDoc(userRef, data);
+      console.log("success!");
+    } catch (e) {
+      console.log("Error is ", e);
+    }
+  } else {
+    console.log("user with the uid of ", uid, " already exists");
+  }
+};
+
+const editUser = async () => {};
+
+const addPost = async (uid, text, date, tag, picture, name) => {
+  try {
+    await addDoc(collection(db, "posts"), {
+      uid: uid,
+      text: text,
+      date: date,
+      tag: tag,
+      picture: picture,
+      name: name,
+    });
+  } catch (error) {
+    console.log("encountered error when adding a post: ", error);
+  }
+};
+
+const deleteUser = async () => {};
+
+export { getUserData, addUser, editUser, deleteUser, addPost };
